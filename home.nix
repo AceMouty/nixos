6b3076cea9e44a2196612a937e74cd8247fedfbb
Kyle G <kyleguerrero85@gmail.com>
2025-01-12 19:41:24 -0600

    chore: add nvf settings

--- home.nix
+++ home.nix
@@ -1,58 +1,63 @@
-{ config, pkgs, lib, ... }: {
-
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: {
   nixpkgs.config = {
     allowUnfree = true;
   };
 
-	home  = {
-		username = "ace";
-		homeDirectory = "/home/ace";
-		stateVersion = "24.11";
-		packages = with pkgs; [ 
-		  flameshot
-		  brave
-	    bat
-		  git
+  home = {
+    username = "ace";
+    homeDirectory = "/home/ace";
+    stateVersion = "24.11";
+    packages = with pkgs; [
+      flameshot
+      brave
+      bat
+      git
       dotnetCorePackages.sdk_8_0_3xx
       jetbrains.rider
       vscode
       go
-		];
-	};
+    ];
+  };
 
-	programs.bash = {
-		enable = true;
-		shellAliases = {
-			rebuild = "sudo nixos-rebuild switch";
-			hms = "home-manager switch";
-		};
+  programs.bash = {
+    enable = true;
+    shellAliases = {
+      rebuild = "sudo nixos-rebuild switch";
+      hms = "home-manager switch";
+    };
     initExtra = ''
       export PATH="$PATH:$HOME/go/bin/"
     '';
-    
-	};
+  };
 
+  #programs.neovim = {
+  #	enable = true;
+  #	defaultEditor = true;
+  #	vimAlias = true;
+  #	vimdiffAlias = true;
+  #	withNodeJs = false;
+  # };
 
-	#programs.neovim = {
-	#	enable = true;
-	#	defaultEditor = true;
-	#	vimAlias = true;
-	#	vimdiffAlias = true;
-	#	withNodeJs = false;
-	# };
-
-	programs.nvf = {
-	  enable = true;
+  programs.nvf = {
+    enable = true;
     enableManpages = true;
-	  settings = {
-	    vim.viAlias = true;
-	    vim.vimAlias = true;
+    settings = {
+      vim.viAlias = true;
+      vim.vimAlias = true;
       vim.useSystemClipboard = true;
-	    
-	    vim.languages.enableLSP = true;
-	    vim.languages.enableTreesitter = true;
+
+      vim.languages.enableLSP = true;
+      vim.languages.enableTreesitter = true;
+      vim.languages.enableFormat = true;
+      vim.lsp.formatOnSave = true;
+
       vim.languages.nix.enable = true;
-	    vim.languages.go.enable = true;
+      vim.languages.go.enable = true;
       vim.languages.lua.enable = true;
 
       vim.globals = {
@@ -68,11 +73,11 @@
 
       vim.keymaps = [
         {
-         key = "jk";
-         mode = "i";
-         silent = true;
-         noremap = true;
-         action = "<Esc>";
+          key = "jk";
+          mode = "i";
+          silent = true;
+          noremap = true;
+          action = "<Esc>";
         }
       ];
 
@@ -88,6 +93,6 @@
           open = "<c-j>";
         };
       };
-	  };
-	};
+    };
+  };
 }
